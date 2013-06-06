e43743513bb8175ba63ed8ff86a913bb7d4a59de
MAYAH <mayah@mayah.jp>
2013-06-06 13:12:21 +0900

    Removed warnings

--- app/sessions/TwitterLoginInformation.scala
+++ app/sessions/TwitterLoginInformation.scala
@@ -2,8 +2,10 @@ package sessions
 import twitter4j.Twitter
 import twitter4j.auth.RequestToken
 
-@serializable
-class TwitterLoginInformation(val twitter: Twitter, val requestToken: RequestToken, val redirectURL: Option[String]) {
+class TwitterLoginInformation(
+    val twitter: Twitter,
+    val requestToken: RequestToken,
+    val redirectURL: Option[String]) extends Serializable {
   def authenticationURL = requestToken.getAuthenticationURL()
 }
 
